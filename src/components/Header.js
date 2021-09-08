d640c07e2a0d7c748286194707000f1b6803cc73
Iaiky <iaikitiananaej@gmail.com>
2021-09-08 09:20:50 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router'
+import Button from "./Button"
+
+const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
+
+    return (
+        <header className="header">
+            <h1 >{title}</h1>
+            {location.pathname === '/' && (<Button 
+                color = {showAdd ? 'red' : 'green'} 
+                text = {showAdd ? 'close' : 'Add'} 
+                onClick = {onAdd}
+            />)}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title : "Hello Track",
+}
+
+Header.propTypes = {
+    title : PropTypes.string
+}
+
+export default Header
